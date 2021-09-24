133e8b76dbd32ab0f6d42d88e371b952ee7e4faa
Rodrigo Lima <rodsl@icloud.com>
2021-09-23 21:58:14 -0300

    Implemented like and dislike text count

--- js/scriptFeed.js
+++ js/scriptFeed.js
@@ -9,17 +9,40 @@ verMaisBtn.addEventListener("click", (e) => {
   verMaisBtn.insertAdjacentElement("beforebegin", postClone);
 });
 
-const notShowLike = document.querySelector(".not-show-like");
-const showLike = document.querySelector(".show-like");
-const qtdLikes = document.querySelector("#qtdLikes");
+const likeAPost = (like) => {
+  const likes = like.nextElementSibling.querySelector("#qtdLikes");
+  if (!like.classList.toggle("like")) {
+    likes.innerHTML = Number(likes.innerHTML) - 1;
+  }
+  if (!like.classList.toggle("not-like")) {
+    likes.innerHTML = Number(likes.innerHTML) + 1;
+  }
+};
 
-showLike.addEventListener("click", (e)=> {
-  showLike.classList.toggle("show-like")
-  notShowLike.classList.toggle("show-like")
-  qtdLikes.innerHTML = Number(qtdLikes.innerHTML) + 1
-})
-notShowLike.addEventListener("click", (e)=> {
-  notShowLike.classList.toggle("show-like")
-  showLike.classList.toggle("show-like")
-  qtdLikes.innerHTML = Number(qtdLikes.innerHTML) - 1
-})
+const fetchPosts = () => {
+  const post = document.querySelector("div.card");
+  const postClone = post.cloneNode(true);
+  verMaisBtn.insertAdjacentElement("beforebegin", postClone);
+};
+
+// Scroll infinito
+window.addEventListener("scroll", (e) => {
+  const { innerHeight, scrollY } = window;
+  const {
+    body: { offsetHeight },
+  } = document;
+  if (innerHeight + scrollY >= offsetHeight) {
+    fetchPosts();
+  }
+});
+
+const originalBody = document.body.cloneNode(true)
+window.addEventListener("mousemove", (e) => {
+  const { screenY, clientY } = e;
+  if (clientY < 100) {
+    document.body.innerHTML = `<div class="d-flex justify-content-center align-content-center
+    "><h1>JÁ VAI EMBORA NÉ?</h1></div>`
+  } else if (clientY > 150) {
+    document.body.innerHTML = originalBody.outerHTML
+  }
+});
